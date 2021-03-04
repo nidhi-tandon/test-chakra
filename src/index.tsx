22330ab8a1fdb4edf7044688e032b5d9480475ea
Nidhi Tandon <>
2021-03-04 15:19:23 +0530

    feat: add types for theme and colors

--- src/index.tsx
+++ src/index.tsx
@@ -1,86 +1,84 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {ChakraProvider} from "@chakra-ui/react"
-import {extendTheme} from "@chakra-ui/react"
+import {ChakraProvider, Theme, extendTheme, Colors} from "@chakra-ui/react"
 import CardComponent from "./CardComponentFold";
 
-const colors = {
-  background: {
-    200: "#f0ecf1",
-    pink:{
-      800: "#ee5a6c"
+const colors: Colors = {
+    background: {
+        200: "#f0ecf1",
+        pink: {
+            800: "#ee5a6c"
+        },
+        blue: {
+            800: "#04093f"
+        }
     },
-    blue:{
-      800: "#04093f"
-    }
-  },
-  color: {
-    800: "#211b23",
-    700: "#515978",
-    200: "#ffffff"
-  },
+    color: {
+        800: "#211b23",
+        700: "#515978",
+        200: "#ffffff"
+    },
 }
 
 const fontSizes = {
-  xs: "10px",
-  sm: "12px",
-  md: "14px",
-  lg: "16px",
-  xxl: "26px"
+    xs: "10px",
+    sm: "12px",
+    md: "14px",
+    lg: "16px",
+    xxl: "26px",
 }
 
 const Form = {
-  parts: ["form", "input"],
-  baseStyle: {
-    form: {
-      boxShadow: "lg",
-      flexDirection: "column",
+    parts: ["form", "input"],
+    baseStyle: {
+        form: {
+            boxShadow: "lg",
+            flexDirection: "column",
+        },
+        input: {
+            fontWeight: "medium",
+            lineHeight: "normal",
+            color: "gray.600",
+            backgroundColor: "white",
+            borderRadius: 0,
+        },
     },
-    input: {
-      fontWeight: "medium",
-      lineHeight: "normal",
-      color: "gray.600",
-      backgroundColor: "white",
-      borderRadius: 0,
+    size: {
+        sm: {
+            input: {
+                fontSize: "0.75rem",
+                px: 2,
+                py: 1,
+            },
+        },
+        md: {
+            input: {
+                fontSize: "0.875rem",
+                px: 3,
+                py: 2,
+            },
+        },
     },
-  },
-  size: {
-    sm: {
-      input: {
-        fontSize: "0.75rem",
-        px: 2,
-        py: 1,
-      },
+    defaultProps: {
+        size: "md",
+        variant: "primary"
     },
-    md: {
-      input: {
-        fontSize: "0.875rem",
-        px: 3,
-        py: 2,
-      },
-    },
-  },
-  defaultProps: {
-    size: "md",
-    variant: "primary"
-  },
 }
 
-
-const theme = extendTheme({
-  colors,
-  fontSizes,
-  components: {
-    Form,
-  }
+const theme: Theme = extendTheme({
+    colors,
+    fontSizes,
+    components: {
+        Form,
+    },
 })
 
 ReactDOM.render(
-  <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <CardComponent/>
-    </ChakraProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <ChakraProvider theme={theme}>
+            <CardComponent/>
+        </ChakraProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
